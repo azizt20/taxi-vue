ef6d170e363caba6d25af7230c762468cdd385de
azizt20 <64777126+azizt20@users.noreply.github.com>
2022-03-31 15:30:38 +0500

    Update OrderForm.vue

--- src/components/OrderForm.vue
+++ src/components/OrderForm.vue
@@ -74,7 +74,7 @@
       </a-form-item>
 
       <a-button @submit.prevent="submitOrder"
-                :disabled="!idLocation || !phoneNomber || !idCategory" 
+                :disabled="!idLocation || !phoneNomber || !idCategory || window.navigator.onLine" 
                 size="large" type="primary" html-type="submit">
         {{ $t('Оформить заказ') }}
       </a-button>
